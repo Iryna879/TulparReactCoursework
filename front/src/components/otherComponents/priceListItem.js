75d5819b37f0dfcd6981583f9fb35ad65aa7dfec
Iryna <irysukd111@gmail.com>
2021-12-18 22:26:43 +0200

    price finish

--- front/src/components/otherComponents/priceListItem.js
+++ front/src/components/otherComponents/priceListItem.js
@@ -1,35 +1,43 @@
 import React, {useEffect, useState} from "react";
-import ReactDataGrid from "react-data-grid";
-
+import ReactDataGrid from "@inovua/reactdatagrid-community";
+import '@inovua/reactdatagrid-community/index.css';
 export function PriceListItem (props){
     const price = props.price;
 const columns = [
-    {key: "name", name:"Назва послуги"},
-    {key: "price", name:"Вартість"}
+    {name: "name", header:"Назва послуги", minWidth:50, defaultFlex: 2},
+    {name: "price", header:"Вартість", maxWidth: 1000, defaultFlex: 1}
 ];
-let rows = [];
-price.map(p => {
-    return rows.push(p.service)
-} );
-console.log("rows: " + rows.length);
+/*let rows = [];
+const map1 = price.map(function(p) {
+    return rows.push(p.title);
+    }
+ );*/
+    let mass = [];
+    price.map(serv => {
 
+        mass.push({name: serv.service, price: serv.price});
+    })
+    let title = mass.slice(0,4);
+console.log("mass: " + mass.length + " " + mass);
+    console.log("title: " + title.length);
 
+const gridStyle = {minHeight: 550}
 
     /*<li>Назва послуги</li>
     <li>Вартість</li>
     <li>Назва послуги</li>
     <li>Вартість</li>*/
 
-    let res = price.map(function (p){
+  /*  let res = price.map(function (p){
         return <ul key={p._id}>
             <li>{p.service}</li>
             <li>{p.price}</li>
             </ul>
-        /*<tr key={p._id}>
+        /!*<tr key={p._id}>
             <td>{p.service}</td>
             <td>{p.price}</td>
-        </tr*/
-    });
+        </tr*!/
+    });*/
 return (
     /*<table >
        <thead>
@@ -49,12 +57,14 @@ return (
         <li>Вартість</li>
         {res}
     </ul>*/
-
-    <ReactDataGrid
+<div>
+   <ReactDataGrid
+       idProperty="id"
         columns={columns}
-        rowGetter={(i) => rows[i]}
-        rowsCount={rows.length}
+        dataSource={mass}
+       style={gridStyle}
         />
-
+<div>Hello</div>
+</div>
 )
 }
